2ded8ee837bee84435c6b800600d5276e0a05291
uchebnick <uchebnick@MacBook-Pro-uchebnick.local>
2026-01-06 17:59:27 +0300

    fix: fix providers list

--- internal/ton/providers.go
+++ internal/ton/providers.go
@@ -8,14 +8,7 @@ import (
 )
 
 var KnownProviders = []string{
-	"EQCtCEcQeulPvo89ohIySxB0XT0RdON_7c9qkziic59G9mg6",
 	"EQDJlWIX-7HcN-7RqDhFhpuCfbHLruOMJ2p4hKKr_AN7_R9S",
-	"EQBQZHQElR4NsG1LpxcgDs28uan8mC4oIRdxJcnmHdYKxKWC",
-	"EQD6bNCmOioAWowK6ee5f0_wt_9ZVEK2_4h0dbSfUqxtJdC3",
-	"EQDSDRwBN8vv0TvGRd9dh7Cq9B_EToqCWNmQHJS7UlDA82y7",
-	"EQDqtIbnphcjx310zHrn7o9FwD-ToWTb1psIH5IDoyEadQsB",
-	"EQDrqJ_AbapwD-38KOI8qJmbLexmmxKbh2ngYs95Lq2A8Uw7",
-	"EQDKX25ZfT6rik45LBXoxXMycfBJDw-TV256wF6ZmNAEjoz0",
 }
 
 func (s *Service) FindRandomProvider(ctx context.Context, exclude []string) (string, error) {
